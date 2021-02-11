eec1f79cba7a66f9fa64ff5bee12e723f6645884
Pragnesh Patel <pragnesh.patel@sifive.com>
2021-02-11 19:31:25 +0530

    Add RPMSG_NO_IPI
    Signed-off-by: Pragnesh Patel <pragnesh.patel@sifive.com>

--- debug.mk
+++ debug.mk
@@ -4,9 +4,9 @@
 
 # Set the optimization level
 RISCV_ASFLAGS += -O0
-RISCV_CFLAGS += -O0
+RISCV_CFLAGS += -O0 -DRPMSG_NO_IPI
 RISCV_CXXFLAGS += -O0
-RISCV_CFLAGS += -I/home/yash/source/openAmp/libmetal/build/install/usr/local/include -I/home/yash/source/openAmp/open-amp/build/install/usr/local/include -L/home/yash/source/openAmp/open-amp/build/install/usr/local/lib/
+RISCV_CFLAGS += -I/home/pragneshp/opensource/libmetal/build-baremetal/install/usr/local/include -I/home/pragneshp/opensource/open-amp/build-baremetal/install/usr/local/include -L/home/pragneshp/opensource/open-amp/build-baremetal/install/usr/local/lib/
 
 # Enable debug
 RISCV_ASFLAGS += -g
